e8afd61f791b174ebdc75d8f1a95713fda1e71cd
henryharris018 <hharri25@aum.edu>
2026-04-19 18:57:22 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+Project Title: The Numbers Station
+Project Members: Henry Harris, Taylor Lindsey, and William Bailey
+Date: 4/19/2026
+
+This project simulates a simple calculator. In this program, you can use addition, division, multiplication, and subtraction to calculate math problems. 
---
+++ The Numberz Station.cpp
@@ -0,0 +1,60 @@
+////////////////////////////////////////////////////////
+////////////    Calculator Program    /////////////////
+//  Taylor Lindsey, William Bailey, Henry Harris    //
+//////////////////// 4/19/26 ////////////////////////
+//                                                 //
+///////////  Input Number Operand Number   /////////
+//////////   Num1 (+ - / *) Num2           ////////
+/////////   Input: 2 num and operand       ///////
+////////    Output: Calculation or Error   //////
+////////////////////////////////////////////////
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char op;
+    char ch = 'y';
+    cout << "Calculator Prime\n";
+
+
+    while (ch == 'y' || ch == 'Y')
+    {
+        double result;
+
+        cout << "Input problem: ";
+        cin >> num1 >> op >> num2;
+
+        if (op == '+')
+            result = num1 + num2;
+        else if (op == '-')
+            result = num1 - num2;
+        else if (op == '*')
+            result = num1 * num2;
+        else if (op == '/')
+        {
+            if (num2 == 0)
+            {
+                cout << "Error: Cannot divide 0\n\n";
+                continue;
+            }
+            result = num1 / num2;
+        }
+
+        else
+        {
+            cout << "Letter or invalid operand detected.\n";
+            continue;
+        }
+        cout << "Result: " << result << endl;
+        cout << endl;
+
+        cout << "Would you like to continue?(y/n): ";
+        cin >> ch;
+    }
+
+    cout << "\nProgram ended!\n\n";
+}
